83312ad1b5f1e99eea0782d6ecf358e6817621f6
bryan rojas <rojaschavez929@gmail.com>
2024-10-10 11:44:56 -0500

    agregue el campo de imagen y red social, se modifico NegocioController y el modelo Negocio, tambien la migracion de esta tabla

--- app/Http/Controllers/Api/V1/NegocioController.php
+++ app/Http/Controllers/Api/V1/NegocioController.php
@@ -30,7 +30,24 @@ class NegocioController extends Controller
      */
     public function store(Request $request)
     {
+        $carpeta = "negocio";
+        $ruta = public_path($carpeta);
+        if(!\File::isDirectory($ruta)){
+            $publicPath = 'storage/' . $carpeta;
+            \File::makeDirectory($publicPath,0777,true,true);
+        }
+        $files = $request->file('imagen');
+
         $negocio = new Negocio;
+
+        if($request->hasFile('imagen')){
+            $nombre = uniqid() . '.' . $files->getClientOriginalName();
+            $path = $carpeta . '/' . $nombre;
+            \Storage::disk('public')->put($path, \File::get($files));
+            $negocio->imagen = $nombre;
+
+        }
+
         $negocio->nombrenegocio = $request->nombrenegocio;
         $negocio->ruc = $request->ruc;
         $negocio->direccion = $request->direccion;
@@ -43,6 +60,7 @@ class NegocioController extends Controller
         $negocio->lote = $request->lote;
         $negocio->fecha = $request->fecha;
         $negocio->razonsocial = $request->razonsocial;
+        $negocio->redsocial = $request->redsocial;
         $negocio->subcategoria_id = $request->subcategoria_id;
         $negocio->administrado_id = $request->administrado_id;
         $negocio->actividad_economica_id = $request->actividad_economica_id;
@@ -69,10 +87,28 @@ class NegocioController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $carpeta = "negocio";
+        $id = $request->id;
+        $negocio = Negocio::find($id);
+
         $negocio = Negocio::find($id);
         if(!$negocio){
             return response()->json(['message'=>'Negocio no encontrado'],404);
         }
+
+        if ($request->hasFile('imagen')){
+            $nombreArchivo = $negocio -> imagen;
+            $files = $request->file('imagen');
+
+            \Storage::disk('public')->delete($carpeta . '/' . $nombreArchivo);
+            $nombreNuevo = uniqid() . '.' . $files->getClientOriginalName();
+            $pathNuevo = $carpeta . '/' . $nombreNuevo;
+            \Storage::disk('public')->put($pathNuevo, \File::get($files));
+            $negocio->imagen =  $nombreNuevo;
+        }
+
+
+
         $negocio->nombrenegocio = $request->nombrenegocio;
         $negocio->ruc = $request->ruc;
         $negocio->direccion = $request->direccion;
@@ -84,6 +120,7 @@ class NegocioController extends Controller
         $negocio->manzana = $request->manzana;
         $negocio->lote = $request->lote;
         $negocio->razonsocial = $request->razonsocial;
+        $negocio->redsocial = $request->redsocial;
         $negocio->subcategoria_id = $request->subcategoria_id;
         $negocio->administrado_id = $request->administrado_id;
         $negocio->actividad_economica_id = $request->actividad_economica_id;
@@ -114,6 +151,30 @@ class NegocioController extends Controller
         }
 
     return Excel::download(new NegociosExport($negocio), 'certificado.xlsx');
+    }
+
+    public function buscar()
+    {   
+        
+        $query= Negocio::with('subcategoria','administrado','actividad_economica');
+
+
+
+        if(request()->filled('nombrenegocio')){
+            $nombrenegocio = request()->input('nombrenegocio');
+            $query->where(function ($q) use ($nombrenegocio){
+                $q->where('nombrenegocio','like','%' . $nombrenegocio . '%');
+            });
+        }
+
+        if(request()->filled('subcategoria_id')){
+            $query->where('subcategoria_id', request()->input('subcategoria_id'));
+        }
+
+        $datos = $query ->paginate(10);
+
+        return $datos;
+
     }
 
 }
